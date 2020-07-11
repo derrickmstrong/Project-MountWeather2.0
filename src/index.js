083d824ff5a46a2997c7e59bdd49c5c3063ac1ac
Derrick Strong <derrickmstrong@gmail.com>
2020-07-11 07:02:57 -0500

    Updated components

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import ByCity from './components/ByCity';
-import ByCityId from './components/ByCityId';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <ByCity />
-    <ByCityId />
   </React.StrictMode>,
   document.getElementById('root')
 );
